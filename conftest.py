a657f33a8e8846c7eb0c7262ed37e42a2d7739b8
Arthur <amanasyan@macadamian.com>
2020-03-27 20:08:01 +0400

    adjust html report to create results folder and show screenshots

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -9,6 +11,8 @@ from pages.login_page import LoginPage
 from pages.header_page import Header
 
 CONFIG_PATH = 'common/config/config.yaml'
+RESULTS_DIR = 'results'
+SCREENSHOTS_DIR = 'screenshots'
 
 
 @pytest.fixture(scope="session")
@@ -79,12 +83,18 @@ def driver(browser, url, headless, config):
     print("Driver closed")
 
 
+# region html report
+def _create_results_dir():
+    Path(os.path.join(RESULTS_DIR, SCREENSHOTS_DIR)).mkdir(parents=True, exist_ok=True)
+
+
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
     """
     Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
     :param item:
     """
+    _create_results_dir()
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
@@ -93,17 +103,21 @@ def pytest_runtest_makereport(item):
     if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
-            if file_name:
+            file_path_in_report = os.path.join(SCREENSHOTS_DIR, f"{report.head_line.replace('.', '-')}.png")
+            file_path = os.path.join(RESULTS_DIR, file_path_in_report)
+            _capture_screenshot(file_path)
+            if file_path_in_report:
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_path_in_report
                 extra.append(pytest_html.extras.html(html))
         report.extra = extra
 
 
 def _capture_screenshot(name):
     driver_.get_screenshot_as_file(name)
+
+
+# endregion
 
 
 @pytest.fixture()
